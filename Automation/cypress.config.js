17b884ec2c3fff25d15db8ed6da814d11d1dd3fa
specifiedram98871 <ramkm98871@gmail.com>
2026-03-15 19:28:21 +0545

    fix: url and added env file

--- Automation/cypress.config.js
+++ Automation/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL || "http://localhost:5173",
+    baseUrl: process.env.BASE_URL || "https://7thsemesterproject.vercel.app",
     supportFile: "cypress/support/e2e.js",
     video:true,
     screenshotsFolder:"cypress/screenshots",
