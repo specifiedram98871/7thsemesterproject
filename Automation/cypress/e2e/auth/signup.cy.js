2165e1ac94ddf7d731b72bd9272d116c791f262a
specifiedram98871 <ramkm98871@gmail.com>
2026-03-15 17:34:14 +0545

    feat: add e2e test for registration functionality

--- Automation/cypress/e2e/auth/signup.cy.js
+++ Automation/cypress/e2e/auth/signup.cy.js
@@ -1,3 +1,5 @@
+import { generateRandomEmail } from "../../support/utils/utils";
+
 describe("Registration functionality", () => {
     before(() => {
         cy.visit("/login");
@@ -19,11 +21,56 @@ describe("Registration functionality", () => {
             cy.url().should("include", "/register");
             cy.get('h1').should("have.text", "Looks like you're new here!");
         })
-        // it("Verify registered user cannot register again",()=>{
-        //    cy.fixture("signedupUser").then((user)={
 
-        //    })
-        // })
+        it("Verify registered user cannot register again", () => {
+            cy.fixture("signup/signedupUser").then((user) => {
+                cy.get('input[name="name"]').type("TestUser");
+                cy.get('input[name="email"]').type(user.email);
+                cy.get('input[name="password"]').type(user.password);
+                cy.get('input[name="cpassword"]').type(user.password);
+                cy.get('input[name="gender"][value = "male"]').check().should('be.checked');
+                cy.get('input[name="avatar"]').selectFile('assets/test.jpg', { force: true });
+                cy.get('button[type="submit"]').contains('Signup').click();
+                cy.get('#notistack-snackbar').should('have.text', 'Duplicate email entered');  
+            })
+        })
+
+        it("Verify password length must be 8 character in length", ()=>{
+            cy.fixture("signup/signedupUser").then((user) => {
+                cy.get('input[name="name"]').type("TestUser");
+                cy.get('input[name="email"]').type(user.email);
+                cy.get('input[name="gender"][value = "male"]').check().should('be.checked');
+                cy.get('input[name="password"]').type("user");
+                cy.get('input[name="cpassword"]').type("user");
+                cy.get('button[type="submit"]').contains('Signup').click();
+                cy.get('#notistack-snackbar').should('have.text', 'Password length must be atleast 8 characters'); 
+            })
+        })
+
+        it("Verify user must select avatar for registration", ()=>{
+            cy.fixture("signup/signedupUser").then((user) => {
+                cy.get('input[name="name"]').type("TestUser");
+                cy.get('input[name="email"]').type(user.email);
+                cy.get('input[name="gender"][value = "male"]').check().should('be.checked');
+                cy.get('input[name="password"]').type(user.password);
+                cy.get('input[name="cpassword"]').type(user.password);
+                cy.get('button[type="submit"]').contains('Signup').click();
+                cy.get('#notistack-snackbar').should('have.text', 'Select Avatar'); 
+            })
+        })
+
+        it("Verify valid and unique email can be registered", () => {
+            cy.fixture("signup/signedupUser").then((user) => {
+                cy.get('input[name="name"]').type("TestUser");
+                cy.get('input[name="email"]').type(generateRandomEmail());
+                cy.get('input[name="password"]').type(user.password);
+                cy.get('input[name="cpassword"]').type(user.password);
+                cy.get('input[name="gender"][value = "male"]').check().should('be.checked');
+                cy.get('input[name="avatar"]').selectFile('assets/test.jpg', { force: true });
+                cy.get('button[type="submit"]').contains('Signup').click();
+                cy.url().should("include", Cypress.env('frontendUrl'));  
+            })
+        })
 
     })
 
