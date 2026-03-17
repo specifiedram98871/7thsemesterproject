c47262553df58f1cb0682a0291748b13bc204978
specifiedram98871 <ramkm98871@gmail.com>
2026-03-17 21:26:46 +0545

    feat: add categories and catNav constants for product categorization

--- Automation/cypress/support/utils/utils.js
+++ Automation/cypress/support/utils/utils.js
@@ -1,3 +1,35 @@
 export function generateRandomEmail(){
     return`test${Date.now()}@test.com`;
 }
+
+export const categories = [
+    "Fresh Produce",
+    "Dairy and Eggs",
+    "Bakery",
+    "Snacks and Beverages",
+    "Grocery Staples",
+    "Packaged Foods",
+    "Frozen Foods",
+    "Personal Care",
+    "Health and Wellness",
+    "Baby Care",
+    "Cleaning Supplies",
+    "Household Essentials",
+    "Organic Foods",
+    "Spices and Condiments",
+    "Instant Meals",
+    "Cooking Oils",
+    "International Foods",
+];
+
+export const catNav = [
+   "Bakery" ,
+   "Packaged Foods" ,
+   "Snacks and Beverages" ,
+   "Household Essentials" ,
+   "Frozen Foods" ,
+   "Dairy and Eggs" ,
+   "International Foods",
+   "Fresh Produce" 
+];
+
