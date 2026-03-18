f1f97b55783a4318bfbe86f6d83058f4b23d02db
specifiedram98871 <ramkm98871@gmail.com>
2026-03-18 11:15:46 +0545

    feat: add mock API commands for user and product data in Cypress tests

--- Automation/cypress/support/commands.js
+++ Automation/cypress/support/commands.js
@@ -14,3 +14,21 @@ Cypress.Commands.add('login', (email, password) => {
     })
  })
  
+Cypress.Commands.add('getMe', () => {
+    cy.intercept('GET', '/api/v1/me', {
+    statusCode: 200,
+    body: { user: null },
+  }).as('getMe')
+})
+
+ Cypress.Commands.add('mockCommonApis', () => {
+  cy.intercept('GET', '/api/v1/products/all', {
+    statusCode: 200,
+    body: { products: [] },
+  }).as('getProducts')
+
+  cy.intercept('GET', '/api/v1/recommend/*', {
+    statusCode: 200,
+    body: { recommendations: [] },
+  }).as('getRecommendations')
+})
