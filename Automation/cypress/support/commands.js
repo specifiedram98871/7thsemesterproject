b2f5d8d84e505157afb2f6dd69b88bc2c9f227a2
specifiedram98871 <ramkm98871@gmail.com>
2026-03-18 22:09:02 +0545

    refactor: improve formatting and consistency in login command implementation

--- Automation/cypress/support/commands.js
+++ Automation/cypress/support/commands.js
@@ -1,27 +1,33 @@
 
-Cypress.Commands.add('login', (email, password) => { 
-    cy.request({
-        method:"POST",
-        url:`${Cypress.env('backendUrl')}/api/v1/login`,
-        body:{
-            email:email,
-            password:password
-        }
-    }).then((res)=>{
-        expect(res.status).to.eq(201)
-        expect(res.body).to.have.property("token")
-        expect(res.body.success).to.eq(true)
-    })
- })
- 
+Cypress.Commands.add('login', (email, password) => {
+  cy.request({
+    method: "POST",
+    url: `${Cypress.env('backendUrl')}/api/v1/login`,
+    body: {
+      email: email,
+      password: password
+    }
+  }).then((res) => {
+    expect(res.status).to.eq(201)
+    expect(res.body).to.have.property("token")
+    expect(res.body.success).to.eq(true)
+  })
+})
+
+Cypress.Commands.add("GUIlogin", (email, password) => {
+  cy.get('#email').type(email);
+  cy.get('#password').type(password);
+  cy.contains('button', 'Login').click();
+})
+
 Cypress.Commands.add('getMe', () => {
-    cy.intercept('GET', '/api/v1/me', {
+  cy.intercept('GET', '/api/v1/me', {
     statusCode: 200,
     body: { user: null },
   }).as('getMe')
 })
 
- Cypress.Commands.add('mockCommonApis', () => {
+Cypress.Commands.add('mockCommonApis', () => {
   cy.intercept('GET', '/api/v1/products/all', {
     statusCode: 200,
     body: { products: [] },
