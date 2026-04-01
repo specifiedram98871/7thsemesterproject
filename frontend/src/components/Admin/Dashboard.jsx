a3ca39768eaf0faa76992e40b5bf5d8396f078d6
specifiedram98871 <ramkm98871@gmail.com>
2026-04-01 21:36:20 +0545

    refactor: update Dashboard and Sidebar components for improved responsiveness and code clarity

--- frontend/src/components/Admin/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard.jsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Sidebar from './Sidebar/Sidebar';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Dashboard = ({ activeTab, children }) => {
 
-    const [onMobile, setOnMobile] = useState(false);
     const [toggleSidebar, setToggleSidebar] = useState(false);
-
-    useEffect(() => {
-        if (window.innerWidth < 600) {
-            setOnMobile(true);
-        }
-    }, [])
 
     return (
         <>
             <main className="flex min-h-screen mt-14 sm:min-w-full">
 
-                {!onMobile && <Sidebar activeTab={activeTab} />}
-                {toggleSidebar && <Sidebar activeTab={activeTab} setToggleSidebar={setToggleSidebar}/>}
+                <div className="hidden sm:block">
+                    <Sidebar activeTab={activeTab} />
+                </div>
+
+                {toggleSidebar && (
+                    <div className="sm:hidden">
+                        <Sidebar activeTab={activeTab} setToggleSidebar={setToggleSidebar} />
+                    </div>
+                )}
 
                 <div className="w-full sm:w-4/5 sm:ml-72 min-h-screen">
                     <div className="flex flex-col gap-6 sm:m-8 p-2 pb-6 overflow-hidden">
