b2def4025a513070bab8e119941f59ae902f4123
specifiedram98871 <ramkm98871@gmail.com>
2026-04-05 18:28:09 +0545

    feat: enhance cypress configuration with reporter options and formatting

--- Automation/cypress.config.js
+++ Automation/cypress.config.js
@@ -4,16 +4,27 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://7thsemesterproject.vercel.app",
     supportFile: "cypress/support/e2e.js",
-    video:true,
-    screenshotsFolder:"cypress/screenshots",
-    videosFolder:"cypress/videos",
+    video: true,
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: true, // to overwrite existing report files
+      html: true,
+      json: true,
+      charts: true,
+      embeddedScreenshots: true,
+      inlineAssets: true,
+    },
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
     defaultCommandTimeout: 9900,
     retries: {
       runMode: 3,
       openMode: 0,
     },
     setupNodeEvents(on, config) {
-      
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
     },
   },
 });
