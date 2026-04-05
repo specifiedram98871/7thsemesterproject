523d6f4d9e0d176a67f1bd81fab9fbb1fcc50e1a
specifiedram98871 <ramkm98871@gmail.com>
2026-04-05 08:27:41 +0545

    feat: add account page functionality tests for user access and navigation

---
+++ Automation/cypress/e2e/pages/accountPage.cy.js
@@ -0,0 +1,48 @@
+describe("Account Page functionality", () => {
+    context("Account page accessibility without login", () => {
+
+        it("TC_ACC_001:Verify /account page is accessible only to logged-in users.", () => {
+            cy.visit("/account");
+            cy.url().should("include", "/login");
+            cy.wait(1000);
+            cy.contains(".SnackbarItem-message", "Please Login to Access").should("be.visible");
+        })
+    })
+
+    context("Account page functionality for logged-in users", () => {
+        beforeEach(() => {
+            cy.mockCommonApis();
+            cy.fixture('login/loginData').then((data) => {
+                const { email, password } = data;
+                cy.login(email, password);
+            });
+            cy.visit("/account");
+        })
+
+        it("TC_ACC_002:Verify /account page is accessible only to logged-in users.", () => {
+            cy.url().should("include", "/account");
+            cy.wait(1000);
+            cy.contains("Personal Information").should("be.visible");
+        })
+
+        it("TC_ACC_003:Verify user profile avatar/icon is displayed in the sidebar.", () => {
+            cy.get("img[alt='Avatar']").should("be.visible");
+        })
+
+        it("TC_ACC_005:Verify [MY ORDERS] menu item navigates to the orders page.", () => {
+            cy.contains("MY ORDERS").click();
+            cy.url().should("include", "/orders");  
+        })
+
+        it("TC_ACC_006:Verify [MY WISHLIST] menu item navigates to the wishlist page.", () => {
+            cy.contains("My Wishlist").click();
+            cy.url().should("include", "/wishlist");
+        })  
+
+        it("TC_ACC_010:Verify [Logout] button logs the user out and redirects to login or home page.", () => {
+            cy.contains("Logout").click();
+            cy.url().should("not.include", "/account");
+            cy.url().should("include", "/login");
+        })
+    })
+})
