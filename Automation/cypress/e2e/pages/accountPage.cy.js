7da246fda764561728030bf1a5835905ef224bdd
specifiedram98871 <ramkm98871@gmail.com>
2026-04-07 21:34:58 +0545

    feat: enhance account page tests with additional assertions for logout and personal information

--- Automation/cypress/e2e/pages/accountPage.cy.js
+++ Automation/cypress/e2e/pages/accountPage.cy.js
@@ -31,18 +31,38 @@ describe("Account Page functionality", () => {
 
         it("TC_ACC_005:Verify [MY ORDERS] menu item navigates to the orders page.", () => {
             cy.contains("MY ORDERS").click();
-            cy.url().should("include", "/orders");  
+            cy.url().should("include", "/orders");
         })
 
         it("TC_ACC_006:Verify [MY WISHLIST] menu item navigates to the wishlist page.", () => {
             cy.contains("My Wishlist").click();
             cy.url().should("include", "/wishlist");
-        })  
+        })
 
         it("TC_ACC_010:Verify [Logout] button logs the user out and redirects to login or home page.", () => {
             cy.contains("Logout").click();
             cy.url().should("not.include", "/account");
             cy.url().should("include", "/login");
+        })
+
+        it("TC_ACC_011:Verify [Logout] clears the user session and prevents access to /account.", () => {
+            cy.contains("Logout").click();
+            cy.url().should("not.include", "/account");
+        })
+
+        it("TC_ACC_012:Verify Personal Information section is displayed on the account page.", () => {
+            cy.contains("Personal Information").should("be.visible");
+            cy.contains("First Name").should("be.visible");
+            cy.contains("Last Name").should("be.visible");
+            //gender is pre-selected
+            cy.get("input[id=male]").should("be.checked");
+            cy.contains("Email Address").should("be.visible");
+        })
+
+        it("TC_ACC_017:Verify [Edit] button is displayed next to Personal Information heading and clickable.", () => {
+            cy.contains("Personal Information").should("be.visible");
+            cy.contains("Edit").should("be.visible").click();
+            cy.url().should("include", "/account/update");
         })
     })
 })
